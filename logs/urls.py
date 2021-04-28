0bd9d1d3a110897332e9384e1d7053c8462bc47d
Ryan <randerson5726@gmail.com>
2021-04-28 15:11:07 -0400

    put update

--- logs/urls.py
+++ logs/urls.py
@@ -3,5 +3,5 @@ from logs import views
 
 urlpatterns = [
     url('api/processing/success', views.get_success_files),
-    url('api/processing/update', views.update_log)
+    url('api/processing/put', views.put_process_log)
 ]
--- logs/views.py
+++ logs/views.py
@@ -19,16 +19,16 @@ def get_success_files(request):
 #@csrf_exempt
 @permission_classes([IsAuthenticated])
 @api_view(['PUT'])
-def update_log(request):
+def put_process_log(request):
     directory = request.GET['DIRECTORY']
     payload = json.loads(request.body)
     try:
         log_item = file_processing.objects.filter(DIRECTORY=directory)
-        # returns 1 or 0
-        log_item.update(**payload)
-
-        return JsonResponse({'status': 'updated'}, safe=False, status=status.HTTP_200_OK)
-    except ObjectDoesNotExist as e:
-        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+        if log_item:
+            log_item.update(**payload)
+            return JsonResponse({'status': 'updated'}, safe=False, status=status.HTTP_200_OK)
+        else:
+            file_processing.objects.create(DIRECTORY=directory, **payload)
+            return JsonResponse({'status': 'added'}, safe=False, status=status.HTTP_200_OK)
     except Exception:
         return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
