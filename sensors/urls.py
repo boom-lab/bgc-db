d717a7e4d3c5c2faa80fe3a0fb3b14c1ca74604a
Ryan <randerson5726@gmail.com>
2021-04-30 10:34:03 -0400

    suna cal rework

--- sensors/urls.py
+++ sensors/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url 
-#from django.urls import path
-from .views import GetSensors, update_suna_cal
+from django.urls import path
+from .views import GetSensors, UpdateSensors
  
 urlpatterns = [ 
     url(r'^api/sensors$', GetSensors.as_view()),
-    url('api/sensors/sunacal', update_suna_cal),
+    path('api/sensors/update/<int:SENSOR_SERIAL_NO>', UpdateSensors.as_view()),
 ]
--- sensors/views.py
+++ sensors/views.py
@@ -16,16 +16,10 @@ class GetSensors(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filter_fields = ['ADD_DATE','SENSOR','SENSOR_MAKER','SENSOR_MODEL','SENSOR_SERIAL_NO','SENSOR_CALIB_DATE']
 
-@permission_classes([IsAuthenticated])
-@api_view(['PUT'])
-def update_suna_cal(request):
-    sn = request.GET['SENSOR_SERIAL_NO']
-    payload = json.loads(request.body)
-    
-    try:
-        obj = get_object_or_404(sensor, SENSOR_SERIAL_NO=sn)
-        obj.PREDEPLOYMENT_CALIB_COEFFICIENT=payload
-        obj.save()
-        return JsonResponse({'status': 'updated'}, safe=False, status=status.HTTP_200_OK)
-    except Exception:
-        return JsonResponse({'status': 'failed'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class UpdateSensors(generics.UpdateAPIView):
+    permission_classes=[permissions.IsAuthenticated]
+    serializer_class = SensorSerializer
+    lookup_field='SENSOR_SERIAL_NO'
+    queryset=sensor.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['ADD_DATE','SENSOR','SENSOR_MAKER','SENSOR_MODEL','SENSOR_SERIAL_NO','SENSOR_CALIB_DATE']
