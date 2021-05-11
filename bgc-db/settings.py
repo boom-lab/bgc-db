a0d20613f99b852b1d12b493e802ebcd19f031d6
Ryan <randerson5726@gmail.com>
2021-05-11 16:08:34 -0400

    allowed hosts update

--- bgc-db/settings.py
+++ bgc-db/settings.py
@@ -38,14 +38,13 @@ if ALLOWED_HOSTS_ENV:
 #For elastic bean stalk health checker
 EC2_PRIVATE_IP = None
 try:
-    EC2_PRIVATE_IP = requests.get(
-        'http://169.254.169.254/latest/meta-data/local-ipv4',
-        timeout=0.01).text
+    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4',timeout=0.01).text
 except requests.exceptions.RequestException:
     pass
 
 if EC2_PRIVATE_IP:
     ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+    print(ALLOWED_HOSTS)
 
 #SECURE_SSL_REDIRECT = True #Uncomment for production
 # SECURE_HSTS_SECONDS = 2592000
