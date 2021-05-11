f7c41ceb4389065e64b39123b6482b4fdcf3ee26
Ryan <randerson5726@gmail.com>
2021-05-11 11:23:00 -0400

    removed throttle classes

--- bgc-db/settings.py
+++ bgc-db/settings.py
@@ -95,15 +95,7 @@ ROOT_URLCONF = 'bgc-db.urls'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
-    ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '1000/day',
-        'user': '1000/day'
-    }
+    ]
 }
 
 
