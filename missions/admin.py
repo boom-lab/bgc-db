0901d8c58c3df49450a479f9aeb8a323ba21b7e8
Ryan <randerson5726@gmail.com>
2021-06-01 17:17:28 -0400

    mission config generator

--- missions/admin.py
+++ missions/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 from .models import mission
+from .views import export_NAVIS_mission_config
 from import_export.admin import ExportMixin
 
 def duplicate_record(modeladmin, request, queryset):
     for object in queryset:
         object.id = None
         object.save()
+
 duplicate_record.short_description = "Duplicate selected missions"
+
+
+def config_file(modeladmin, request, queryset):
+    ids = []
+    for record in queryset:
+        ids.append(record.id)
+
+    return export_NAVIS_mission_config(request, ids)
+
+
+config_file.short_description = "Export Mission Config File"
 
 
 class MissionAdmin(ExportMixin, admin.ModelAdmin):
@@ -17,7 +30,7 @@ class MissionAdmin(ExportMixin, admin.ModelAdmin):
     search_fields = ('DEPLOYMENT',)
     list_per_page = 25
     list_filter = ('DEPLOYMENT',)
-    actions = [duplicate_record]
+    actions = [duplicate_record, config_file]
 
 
 admin.site.register(mission, MissionAdmin)
--- missions/views.py
+++ missions/views.py
@@ -1,18 +1,55 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from jinja2 import Environment, FileSystemLoader
-
+import zipfile
 from .models import mission
+from django.core import serializers
+
+def navis_crc(command_string):
+  crc = 0x1D0F; # start value
+  for c in command_string:
+    crc = crc ^ (ord(c) * 256)
+    for i in range(8,0,-1):
+      if (crc & 0x8000):
+        crc = (crc*2) ^ 0x1021
+      else:
+        crc = (crc*2)
+  return crc & 0xffff
+
+def gen_commands(data):
+    #Create mission configuration command string
+    output = ""
+    for key, value in data.items():
+        if key not in ['DEPLOYMENT','ADD_DATE','COMMENTS']: #Ignore these fields
+            if value: #Ignore blank entries
+                command = key + "(" + str(value) + ")" #command
+                crc = '%04X' % navis_crc(command) #checksum
+                output = output + command + "[0x" + str(crc) + "]" + "\n" #add to output as new line
+    return output
 
 #Create NAVIS mission config file
-def export_NAVIS_mission_config(request, entry_id):
+def export_NAVIS_mission_config(request, entry_ids):
     """Tested with NAVIS_EBR float"""
 
-    #Get data from mission
-    m = get_object_or_404(mission, pk=entry_id)
-
-    output = "Something\nTest"
-
-    response = HttpResponse(output, content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="{}_{}.meta"'.format('test', 'test')
-    return response
+    if len(entry_ids) == 1: #If only one record selected
+        #Get data from mission
+        m = get_object_or_404(mission, pk=entry_ids[0])
+        data = serializers.serialize("python", [m])[0]['fields']
+        output = gen_commands(data)
+       
+        #Return file
+        response = HttpResponse(output, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="{}_{}.cfg"'.format(m.DEPLOYMENT.FLOAT_SERIAL_NO, m.ADD_DATE.strftime("%Y_%m_%d"))
+        return response
+    else:
+        response = HttpResponse(content_type='application/zip')
+        zf = zipfile.ZipFile(response, 'w')
+        for entry in entry_ids: #loop through each mission record selected
+            m = get_object_or_404(mission, pk=entry)
+            data = serializers.serialize("python", [m])[0]['fields']
+            output = gen_commands(data)
+            filename="{}_{}.cfg".format(m.DEPLOYMENT.FLOAT_SERIAL_NO, m.ADD_DATE.strftime("%Y_%m_%d"))
+            zf.writestr(filename, output)
+        zf.close()
+        response['Content-Disposition'] = 'attachment; filename={}'.format("configs.zip")
+        return response
