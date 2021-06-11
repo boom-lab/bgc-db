5de03a9a3d7a4601033374c9b2bfc4c422c533d3
Ryan <randerson5726@gmail.com>
2021-06-11 07:30:11 -0400

    cohort latest profile plot

--- pages/diagnostics_plots.py
+++ pages/diagnostics_plots.py
@@ -107,197 +107,217 @@ def update_cohort_latest_plot(request):
 
         #Number of subplot rows based on number of floats
         wmos = data.PLATFORM_NUMBER.unique()
-        n_rows = math.ceil(len(wmos)/2)
+
+        #List to collect plots
+        plot_divs={}
 
         #each float (seperate subplot)
         for i, wmo in enumerate(wmos):
             fig = go.Figure()
 
-            #Current row and col of subplot
-            crt_row = math.floor(i/2)+1
-            crt_col = i%2+1
-
             #subset to one float
             data_sub = data.loc[data.PLATFORM_NUMBER==wmo,:]
             sn = data_sub.reset_index().loc[0, "FLOAT_SERIAL_NO"]
 
-            #Each profile (series)
-            for prof in data.PROFILE_ID.unique():
-                #Salinity
-                fig.add_trace(
-                    go.Scatter(
-                        x=data_sub.loc[data["PROFILE_ID"]==prof, "PSAL"],
-                        y=data_sub.loc[data["PROFILE_ID"]==prof, "PRES"]*-1,
-                        mode='lines',
-                        marker = {
-                            'color': "#FEBD17",
-                        },
-                        #customdata = hov_data,
-                        hovertemplate ='%{x:.3f}',
-                        name="Profile:"+prof,
-                        xaxis="x"
-                    ),
+            #Salinity
+            fig.add_trace(
+                go.Scatter(
+                    x=data_sub.loc[:, "PSAL"],
+                    y=data_sub.loc[:, "PRES"]*-1,
+                    mode='lines',
+                    marker = {
+                        'color': "#FEBD17",
+                    },
+                    #customdata = hov_data,
+                    hovertemplate ='%{x:.3f}',
+                    name="Salinity",
+                    xaxis="x"
+                ),
+            )
+
+            #BBP700
+            fig.add_trace(
+                go.Scatter(
+                    x=data_sub.loc[:, "BBP700"],
+                    y=data_sub.loc[:, "PRES"]*-1,
+                    mode='lines',
+                    marker = {
+                        'color': "#80BF96",
+                    },
+                    #customdata = hov_data,
+                    hovertemplate ='%{x:.3f}',
+                    name="Backscattering",
+                    xaxis="x2"
+                ),
+            )
+
+            #CDOM
+            fig.add_trace(
+                go.Scatter(
+                    x=data_sub.loc[:, "CDOM"],
+                    y=data_sub.loc[:, "PRES"]*-1,
+                    mode='lines',
+                    marker = {
+                        'color': "#023440",
+                    },
+                    #customdata = hov_data,
+                    hovertemplate ='%{x:.3f}',
+                    name="CDOM",
+                    xaxis="x3"
+                ),
+            )
+
+            #Temeprature
+            fig.add_trace(
+                go.Scatter(
+                    x=data_sub.loc[:, "TEMP"],
+                    y=data_sub.loc[:, "PRES"]*-1,
+                    mode='lines',
+                    marker = {
+                        'color': "#c9324e",
+                    },
+                    #customdata = hov_data,
+                    hovertemplate ='%{x:.3f}',
+                    name="Temperature",
+                    xaxis="x4"
+                ),
+            )
+
+            #DOXY
+            fig.add_trace(
+                go.Scatter(
+                    x=data_sub.loc[:, "DOXY"],
+                    y=data_sub.loc[:, "PRES"]*-1,
+                    mode='lines',
+                    marker = {
+                        'color': "#1f77b4",
+                    },
+                    #customdata = hov_data,
+                    hovertemplate ='%{x:.3f}',
+                    name="Dissolved Oxygen",
+                    xaxis="x5"
+                ),
+            )
+
+
+            fig.add_annotation(text="WMO: "+wmo+" SN: "+str(sn),
+                xref="paper", yref="paper", xanchor='center', yanchor='bottom',
+                x=0.12, y=.82, showarrow=False,
                 )
 
-                #BBP700
-                fig.add_trace(
-                    go.Scatter(
-                        x=data_sub.loc[data["PROFILE_ID"]==prof, "BBP700"],
-                        y=data_sub.loc[data["PROFILE_ID"]==prof, "PRES"]*-1,
-                        mode='lines',
-                        marker = {
-                            'color': "#80BF96",
-                        },
-                        #customdata = hov_data,
-                        hovertemplate ='%{x:.3f}',
-                        name="Profile:"+prof,
-                        xaxis="x2"
+            # Formatting
+            fig.update_layout(
+                template = "ggplot2",
+                #yaxis = {'title':"Pressure"},
+                font = {"size":12},
+                height=850,
+                showlegend=False,
+                margin={'t': 0, 'l':0,'r':0,'b':0},
+                yaxis=dict(
+                    domain=[0.15, 0.85]
+                ),
+                xaxis=dict(
+                    title=dict(
+                        text="Practical Salinity",
+                        standoff=0,
                     ),
-                )
-
-                #CDOM
-                fig.add_trace(
-                    go.Scatter(
-                        x=data_sub.loc[data["PROFILE_ID"]==prof, "CDOM"],
-                        y=data_sub.loc[data["PROFILE_ID"]==prof, "PRES"]*-1,
-                        mode='lines',
-                        marker = {
-                            'color': "#023440",
-                        },
-                        #customdata = hov_data,
-                        hovertemplate ='%{x:.3f}',
-                        name="Profile:"+prof,
-                        xaxis="x3"
+                    titlefont=dict(
+                        color="#FEBD17"
                     ),
-                )
-
-                #Temeprature
-                fig.add_trace(
-                    go.Scatter(
-                        x=data_sub.loc[data["PROFILE_ID"]==prof, "TEMP"],
-                        y=data_sub.loc[data["PROFILE_ID"]==prof, "PRES"]*-1,
-                        mode='lines',
-                        marker = {
-                            'color': "#0D6973",
-                        },
-                        #customdata = hov_data,
-                        hovertemplate ='%{x:.3f}',
-                        name="Profile:"+prof,
-                        xaxis="x4"
+                    tickfont=dict(
+                        color="#FEBD17"
                     ),
-                )
-
-                #DOXY
-                fig.add_trace(
-                    go.Scatter(
-                        x=data_sub.loc[data["PROFILE_ID"]==prof, "DOXY"],
-                        y=data_sub.loc[data["PROFILE_ID"]==prof, "PRES"]*-1,
-                        mode='lines',
-                        marker = {
-                            'color': "#1f77b4",
-                        },
-                        #customdata = hov_data,
-                        hovertemplate ='%{x:.3f}',
-                        name="Profile:"+prof,
-                        xaxis="x5"
+                    position=0.15,
+                    range=[35, 36],
+                    showline=True,
+                    linewidth=1,
+                    linecolor="#FEBD17"
+                ),
+                xaxis2=dict(
+                    title=dict(
+                        text="Particle Backscattering at 700 nm (m<sup>-1</sup>)",
+                        standoff=0,
                     ),
-                )
+                    titlefont=dict(
+                        color="#80BF96"
+                    ),
+                    tickfont=dict(
+                        color="#80BF96"
+                    ),
+                    anchor="free",
+                    overlaying="x",
+                    side="bottom",
+                    position=0.08,
+                    range=[0, .001],
+                    showline=True,
+                    linewidth=1,
+                    linecolor="#80BF96"
+                ),
+                xaxis3=dict(
+                    title=dict(
+                        text="Coloured Dissolved Organic Matter (ppb)",
+                        standoff=10,
+                    ),
+                    titlefont=dict(
+                        color="#023440"
+                    ),
+                    tickfont=dict(
+                        color="#023440"
+                    ),
+                    anchor="free",
+                    overlaying="x",
+                    side="bottom",
+                    position=0.0,
+                    range=[0, 3],
+                    showline=True,
+                    linewidth=1,
+                    linecolor="#023440"
+                ),
+                xaxis4=dict(
+                    title=dict(
+                        text="In-situ Temperature (°C)",
+                        standoff=0,
+                    ),
+                    titlefont=dict(
+                        color="#c9324e"
+                    ),
+                    tickfont=dict(
+                        color="#c9324e"
+                    ),
+                    anchor="free",
+                    overlaying="x",
+                    side="top",
+                    position=.85,
+                    range=[0, 15],
+                    showline=True,
+                    linewidth=1,
+                    linecolor="#c9324e"
+                ),
+                xaxis5=dict(
+                    title=dict(
+                        text="Dissolved Oxygen (μmol/kg)",
+                        standoff=0,
+                    ),
+                    titlefont=dict(
+                        color="#1f77b4"
+                    ),
+                    tickfont=dict(
+                        color="#1f77b4"
+                    ),
+                    anchor="free",
+                    overlaying="x",
+                    side="top",
+                    position=.92,
+                    range=[150, 300],
+                    showline=True,
+                    linewidth=1,
+                    linecolor="#1f77b4"
+                ),
+            )
 
+            plot_divs["plot_div"+str(i)] = plot(fig,output_type='div', include_plotlyjs=False, config= {
+                'displaylogo': False, 'modeBarButtonsToRemove':['lasso2d', 'select2d','resetScale2d']})  
 
-            # #Float Label
-            # if crt_col == 1:
-            #     xpos = 0.07
-            # else:
-            #     xpos = 0.62
-
-            # fig.add_annotation(text="WMO: "+wmo+" SN: "+str(sn),
-            #     xref="paper", yref="paper", xanchor='center', yanchor='bottom',
-            #     x=xpos, y=1, showarrow=False,
-            #     )
-
-        # Formatting
-        fig.update_layout(
-            template = "ggplot2",
-            #yaxis = {'title':"Pressure"},
-            font = {"size":15},
-            height=900,
-            showlegend=False,
-            margin={'t': 30, 'l':0,'r':0,'b':0},
-            yaxis=dict(
-                domain=[0.15, 0.85]
-            ),
-            xaxis=dict(
-                title="PSAL",
-                titlefont=dict(
-                    color="#FEBD17"
-                ),
-                tickfont=dict(
-                    color="#FEBD17"
-                ),
-                position=0.15,
-                range=[35, 36]
-            ),
-            xaxis2=dict(
-                title="BBP700",
-                titlefont=dict(
-                    color="#80BF96"
-                ),
-                tickfont=dict(
-                    color="#80BF96"
-                ),
-                anchor="free",
-                overlaying="x",
-                side="bottom",
-                position=0.08,
-                range=[0, .001]
-            ),
-            xaxis3=dict(
-                title="CDOM",
-                titlefont=dict(
-                    color="#023440"
-                ),
-                tickfont=dict(
-                    color="#023440"
-                ),
-                anchor="free",
-                overlaying="x",
-                side="bottom",
-                position=0.0,
-                range=[0, 3]
-            ),
-            xaxis4=dict(
-                title="Temperature (C)",
-                titlefont=dict(
-                    color="#0D6973"
-                ),
-                tickfont=dict(
-                    color="#0D6973"
-                ),
-                anchor="free",
-                overlaying="x",
-                side="top",
-                position=.85,
-                range=[0, 15]
-            ),
-            xaxis5=dict(
-                title="Oxygen",
-                titlefont=dict(
-                    color="#1f77b4"
-                ),
-                tickfont=dict(
-                    color="#1f77b4"
-                ),
-                anchor="free",
-                overlaying="x",
-                side="top",
-                position=.95,
-                range=[150, 300]
-            ),
-        )
-
-        plot_div = plot(fig,output_type='div', include_plotlyjs=False, config= {
-            'displaylogo': False, 'modeBarButtonsToRemove':['lasso2d', 'select2d','resetScale2d']})  
-
-        return JsonResponse({'plot_div': plot_div}, status = 200)
+        return JsonResponse(plot_divs, status = 200)
 
     return JsonResponse({}, status = 400)
