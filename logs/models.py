e1fa2d5d9ec847bbc360aed7912549c1a8cc564a
Ryan <randerson5726@gmail.com>
2021-12-14 09:44:46 -0800

    add error types to tracking table

--- logs/models.py
+++ logs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from deployments.models import deployment
 from choices.models import events
+from choices.models import tracking_error_types
 from django.conf import settings
 
 class Status(models.TextChoices):
@@ -32,6 +33,7 @@ class deployment_tracking(models.Model):
     DATE = models.DateField()
     LOCATION = models.CharField(max_length=100, null=True, blank=True)
     COMMENT = models.TextField(null=True, blank=True)
+    ERROR_TYPE = models.ForeignKey(tracking_error_types, on_delete=models.PROTECT, null=True, blank=True)
     USER = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.PROTECT)
 
 
