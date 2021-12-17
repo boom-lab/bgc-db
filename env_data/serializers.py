a80dd2baf79d248f2b00343bbaed9137ea42e969
Ryan <randerson5726@gmail.com>
2021-12-17 13:09:31 -0800

    get discrete data api

--- env_data/serializers.py
+++ env_data/serializers.py
@@ -13,6 +13,19 @@ class DisProfileSerializer(serializers.ModelSerializer):
         model = discrete_profile
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs): #For gettting request to results column filter of sensor data
+        super(DisProfileSerializer, self).__init__(*args, **kwargs)
+
+        #Filters which fields are returned
+        fields = self.context['request'].query_params.get('output_fields')
+        if fields:
+            fields = fields.split(',')
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
 class ParkSerializer(serializers.ModelSerializer):
  
     class Meta:
