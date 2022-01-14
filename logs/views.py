08de1c725a356d870da3b359b02ee4cd53961b19
randerson57261 <74583143+randerson57261@users.noreply.github.com>
2022-01-14 13:14:40 -0500

    email

--- logs/views.py
+++ logs/views.py
@@ -18,6 +18,49 @@ def get_file_status(request):
 
     return JsonResponse(result)
 
+def send_email(payload):
+    #----------------------Email messages--------------#
+    try:
+        #Error processing or warning
+        if payload['STATUS'] != 'Success' and payload['CYCLE'] != '000':
+            send_mail(
+                'BGC Processing '+payload['STATUS'] + ' - SN: ' + payload['FLOAT_SERIAL_NO'] + ' Cycle: ' + payload['CYCLE'],
+                payload.get('DETAILS'),
+                'from@example.com',
+                ['randerson@whoi.edu'],
+                fail_silently=False,
+            )
+
+        #Mision Prelude
+        if payload['CYCLE'] == '000':
+            if payload['DETAILS']:
+                details = payload['DETAILS']
+            else:
+                details = ""
+            send_mail(
+                payload['FLOAT_SERIAL_NO'] + ": "+"Mission prelude recieved",
+                payload['STATUS'] +" - "+ details,
+                'from@example.com',
+                ['randerson@whoi.edu','dnicholson@whoi.edu'],
+                fail_silently=False,
+            )
+
+        #first cycle
+        if payload['CYCLE'] == '001':
+            if payload['DETAILS']:
+                details = payload['DETAILS']
+            else:
+                details = ""
+            send_mail(
+                payload['FLOAT_SERIAL_NO'] + ": "+"First cycle recieved",
+                payload['STATUS'] +" - "+ details,
+                'from@example.com',
+                ['randerson@whoi.edu','dnicholson@whoi.edu'],
+                fail_silently=False,
+            )
+    except Exception:
+        pass
+
 #@csrf_exempt
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
@@ -25,52 +68,13 @@ def put_process_log(request):
     directory = request.GET['DIRECTORY']
     payload = json.loads(request.body)
     
-    #----------------------Email messages--------------#
-    #Error processing or warning
-    if payload['STATUS'] != 'Success' and payload['CYCLE'] != '000':
-        send_mail(
-            'BGC Processing '+payload['STATUS'] + ' - SN: ' + payload['FLOAT_SERIAL_NO'] + ' Cycle: ' + payload['CYCLE'],
-            payload.get('DETAILS'),
-            'from@example.com',
-            ['randerson@whoi.edu'],
-            fail_silently=False,
-        )
-
-    #Mision Prelude
-    if payload['CYCLE'] == '000':
-        if payload['DETAILS']:
-            details = payload['DETAILS']
-        else:
-            details = ""
-        send_mail(
-            payload['FLOAT_SERIAL_NO'] + ": "+"Mission prelude recieved",
-            payload['STATUS'] +" - "+ details,
-            'from@example.com',
-            ['randerson@whoi.edu','dnicholson@whoi.edu'],
-            fail_silently=False,
-        )
-
-    #first cycle
-    if payload['CYCLE'] == '001':
-        if payload['DETAILS']:
-            details = payload['DETAILS']
-        else:
-            details = ""
-        send_mail(
-            payload['FLOAT_SERIAL_NO'] + ": "+"First cycle recieved",
-            payload['STATUS'] +" - "+ details,
-            'from@example.com',
-            ['randerson@whoi.edu','dnicholson@whoi.edu'],
-            fail_silently=False,
-        )
-
-    #------------------------------------------------#  
     try:
         log_item = file_processing.objects.filter(DIRECTORY=directory)
-        if log_item:
+        if log_item: #Log entry already exists
             log_item.update(**payload)
             return JsonResponse({'status': 'updated'}, safe=False, status=status.HTTP_200_OK)
-        else:
+        else: #Create new entry
+            send_email(payload) #only send email if new record
             file_processing.objects.create(DIRECTORY=directory, **payload)
             return JsonResponse({'status': 'added'}, safe=False, status=status.HTTP_200_OK)
     except Exception:
