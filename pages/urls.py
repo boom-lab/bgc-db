7ae28db14513c500fd235c7d091ee3bc43ccd75c
randerson57261 <74583143+randerson57261@users.noreply.github.com>
2022-07-11 15:09:19 -0400

    URL edits

--- pages/urls.py
+++ pages/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    #New front end - Data table pages
+    #Front end - Data table pages
     path('FE/sensor_qc_data', views.GetSensorQCdata.as_view(), name="sensor_qc_data"),
     path('FE/sn_data', views.serial_number_data, name="sn_data"),
     path('FE/tracking_data', views.GetTrackingData.as_view(), name="tracking_data"),
     path('FE/float_detail', views.float_detail, name="float_detail"),
 
-    #New front end - React - data for plots and map
+    #Front end - data for plots and map
     path('FE/profile_explorer_data', views.profile_explorer_data, name="profile_explorer_data"),
     path('FE/latest_profiles_data', views.latest_profiles_data, name="latest_profiles_data"),
     path('FE/all_profiles_data', views.all_profiles_data, name="all_profiles_data"),
@@ -21,6 +21,6 @@ urlpatterns = [
     path('FE/get_deployments_list', views.get_deployments_list, name = "get_deployments_list"),
 
     #Redirect all other requests to front end site
-    re_path(r'.*',views.newsite, name='newsite'),
+    re_path(r'^(?!FE|api).*',views.newsite, name='newsite'),
     
 ]
